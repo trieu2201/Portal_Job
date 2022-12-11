71b5026118c5ab63ece4878ca2548f3a1a11a220
leloc0609 <80434238+leloc0609@users.noreply.github.com>
2022-12-12 06:37:18 +0700

    commit

--- Home.php
+++ Home.php
@@ -1,28 +1,8 @@
-<<<<<<< Updated upstream
-<div id="container-fluid">
-			<marquee bgcolor= "#FCA317" height="100%" class="welcome">Welcome To JOBIFIND</marquee>
-            <div style ="background-image: url('https://images.unsplash.com/photo-1529400971008-f566de0e6dfc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'); height:500px; width: 100%"> 
-                <div class = "row">
-                    <div class="col-1">
-                    </div>
-                    <div class="col-3 ">
-                        <div class="offset_test">
-                            <p class = "ms-auto title" style = "font-size: 50px;">JOBIFIND</p>
-                            <p>Welcome to online Job Portal. It provides facility to the Job Seeker to search for various jobs as per his qualification. Here Job Seeker can registered himself on the web portal and create his profile along with his educational information. Job Seeker can search various jobs and apply for the Job.</p>
-                            <p>This Portal is also designed for the various employer who required to recruit employees in their organization. Employer can registered himself on the web portal and then he can upload information of various job vacancies in their organization. Employeer can view the applications of Job Seeker and send call latter to the job seekers.</p>
-                        </div>
-
-                    </div>
-                </div>
-               
-            </div>
-            <br>
-=======
 <marquee bgcolor= "#FCA317" height="100%" class="welcome">Welcome To JOB PORTAL</marquee>
 <div>
     <div> 
         <div class="masthead">
->>>>>>> Stashed changes
+
             <div class = "row">
                 <div class="col-1">
 
